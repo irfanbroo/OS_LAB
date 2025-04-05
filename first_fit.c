a58251e9985b4b1e7525c22b255952edfa08e056
irfanbroo <123147439+irfanbroo@users.noreply.github.com>
2025-04-05 18:56:35 +0530

    Update first_fit.c

--- first_fit.c
+++ first_fit.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-void main(){
-    int bsize[10],psize[10],pno,bno,flag[10],allocation[10],i,j;
 
-    for(i=0;i<10;i++){
-        flag[i]=0;
-        allocation[i]=-1;
-    }
-    printf("Enter the number of process: ");
-    scanf("%d",&pno);
-    printf("Enter the number of blocks: ");
-    scanf("%d",&bno);
-    printf("Enter the size of each blocks: ");
-    for(i=0;i<bno;i++){
-        scanf("%d",&bsize[i]);
-    }
-    printf("Enter the size of each process: ");
+void main(){
+    int bno,pno,bsize[100],psize[100],i,j,allocation[100];
+
+    printf("Enter the number of process and number of blocks: ");
+    scanf("%d %d", &pno, &bno);
+    printf("Enter the processes: ");
     for(i=0;i<pno;i++){
         scanf("%d",&psize[i]);
     }
+    printf("Enter the blocks: ");
+    for(i=0;i<bno;i++){
+        scanf("%d",&bsize[i]);
+    }
 
+    for(i=0;i<50;i++){
+        allocation[i] = -1;
+    }
+    
     for(i=0;i<pno;i++){
         for(j=0;j<bno;j++){
             if(psize[i]<=bsize[j]){
-                allocation[i] = j;
+                allocation[i] = bsize[j];
                 bsize[j] -=psize[i];
                 break;
             }
@@ -30,17 +29,27 @@ void main(){
         }
     }
 
-    printf("\n Process number\t Process size\t Block Number \n");
+
+    printf("process Number\t process size\t block size \n");
     for(i=0;i<pno;i++){
-        printf("%d\t\t%d\t\t",i+1,psize[i]);
+        printf("%d\t\t%d\t\t", i+1,psize[i]);
         if(allocation[i]!=-1){
-            printf("%d\n",allocation[i]+1);
+            printf("%d\n",allocation[i]);
         }
         else{
             printf("Not allocated\n");
         }
-
     }
+
+
+
+
+
+
+
+
+
+
 
 
 }
