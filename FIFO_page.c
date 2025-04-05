b5221fc18de9aaf86eee6311e2c3223d8ba90fc4
irfanbroo <123147439+irfanbroo@users.noreply.github.com>
2025-04-05 10:01:17 +0530

    Update FIFO_page.c

--- FIFO_page.c
+++ FIFO_page.c
@@ -1,3 +1,5 @@
+//Samool version of fifo page fault
+
 #include<stdio.h>
 void main() {
     int rs[100], m[100];
@@ -42,4 +44,57 @@ void main() {
         }
     }
     printf("Total number of page faults: %d\n", pagefault);
+}
+
+
+
+
+
+// My ver
+#include <stdio.h>
+void main(){
+    int i,j,k,count=0,pgfault=0,rs[100],ms[100],f,n;
+    printf("Enter the number of reference string: ");
+    scanf("%d",&n);
+    printf("Enter the reference string: ");
+    for(i=0;i<n;i++){
+        scanf("%d",&rs[i]);
+    }
+    printf("Enter the number of frames: ");
+    scanf("%d", &f);
+    for(i=0;i<f;i++){
+        ms[i] = -1;
+    }
+
+
+    for(i=0;i<n;i++){
+        for(j=0;j<f;j++){
+            if (rs[i] == ms[j]){
+                break;
+            }
+                
+        }
+        if (j == f){
+            ms[count++] = rs[i];
+            pgfault++;
+
+        }
+
+        for(j=0;j<f;j++){
+            printf("%d\t",ms[j]);
+        }
+
+        if (j == f){
+            printf("Page fault: %d",pgfault);
+        }
+        printf("\n");
+        if(count == f){
+            count = 0;
+        }
+
+
+    }
+
+    printf("The total number of page fault is %d: ",pgfault);
+
 }
