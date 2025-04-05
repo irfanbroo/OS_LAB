6c3ea04e58e16972100bcd54dd98269ee5b16562
irfanbroo <123147439+irfanbroo@users.noreply.github.com>
2025-04-05 21:26:25 +0530

    Update fcfs_with_arrival_time.c

--- fcfs_with_arrival_time.c
+++ fcfs_with_arrival_time.c
@@ -8,7 +8,7 @@ struct Process{
     int wt;
     int tat;
 }p[20],temp;
-
+int totaltat =0, totalwt =0;
 void main(){
     int i,j,n;
     printf("Enter the number of process: ");
@@ -32,15 +32,15 @@ void main(){
     }
 
     p[0].ct = p[0].bt + p[0].at;
-    for(i=1;i<n;i++){
+    for(i =0;i<n;i++){
         if(p[i].at > p[i-1].ct){
-            p[i].ct = p[i].at + p[i].bt;
+            p[i].ct = p[i].bt + p[i].at;
         }
         else{
-            p[i].ct = p[i-1].ct + p[i].bt;
-
+            p[i].ct = p[i].bt + p[i-1].ct;
         }
-    }
+    } 
+
     for(i=0;i<n;i++){
         p[i].tat = p[i].ct - p[i].at;
         p[i].wt = p[i].tat - p[i].bt;
@@ -50,10 +50,13 @@ void main(){
     for(i=0;i<n;i++){
         printf("%d\t\t%d\t%d\t%d\t%d\t%d\n", 
             p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+        totaltat += p[i].tat;
+        totalwt += p[i].wt;
 
+}
+    printf("\nAverage tat is %.2f: ",(float)totaltat/n);
+    printf("\n Average wt is %.2f", (float)totalwt/n);
 
-    }
 
 
 }
-
